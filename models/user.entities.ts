697fc4a821fad79c5665be7356d899107e61f298
carlosmares <microsystemags@hotmail.com>
2026-01-29 16:26:25 -0600

    feat: implementar modelo User y optimizar ejecución de tests en CI
    - Crear entidad User con ID en formato UUID y roles restringidos (operador, oficial).
    - Agregar hash de contraseña automático con bcrypt y ocultar password en consultas.
    - Documentar modelo User con anotaciones de OpenAPI para Swagger.
    - Corregir bloqueo de pipeline eliminando el flag --watchAll del comando de test.
    - Agregar script 'test:watch' para desarrollo local en package.json.

---
+++ models/user.entities.ts
@@ -0,0 +1,53 @@
+import bcrypt from "bcrypt";
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+/**
+ * @openapi
+ * components:
+ * schemas:
+ * User:
+ * type: object
+ * required:
+ * - email
+ * - password
+ * - role
+ * properties:
+ * id:
+ * type: string
+ * format: uuid
+ * email:
+ * type: string
+ * format: email
+ * role:
+ * type: string
+ * example: "user"
+ */
+@Entity("user")
+export class User {
+
+    @PrimaryGeneratedColumn("uuid")
+    id!: string;
+
+    @Column({ type: "varchar", length: 100, nullable: false, unique: true })
+    email!: string;
+
+    @Column({ type: "varchar", length: 100, nullable: false, select: false })
+    password!: string;
+
+    @Column({
+        type: "enum",
+        enum: ["operador", "oficial"],
+        default: "operador"
+    })
+    role!: "operador" | "oficial";
+
+    // Hook: Se ejecuta antes de insertar en la base de datos
+    @BeforeInsert()
+    @BeforeUpdate()
+    async hashPassword() {
+        if (this.password) {
+            const saltRounds = 10;
+            this.password = await bcrypt.hash(this.password, saltRounds);
+        }
+    }
+}
